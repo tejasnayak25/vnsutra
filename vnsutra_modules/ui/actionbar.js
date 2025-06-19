b2c9244a5c6a10b0a2c0546816cb9e402537aaee
Tejas Nayak <arpithanayak900@gmail.com>
2025-06-19 10:42:45 +0530

    update

--- vnsutra_modules/ui/actionbar.js
+++ vnsutra_modules/ui/actionbar.js
@@ -146,6 +146,7 @@ function actionBar(config, siderect, width, height, fonts, close_square_img, add
     actionrect.add(actionbar, actionbar_border, actionbar_title, actionbar_title_border, addBtn, closeActionMenuBtn, actionContent, scrollContainer);
 
     window.scrollApp = (deltaY) => {
+        console.log(deltaY);
         let value = scrollbar.y() + deltaY;
         if(value < 0) {
             value = 0;
